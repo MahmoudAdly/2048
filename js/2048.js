7e1b6231eaaa94d1a65504127e98e3bc1eb146bf
mahmoud adly <mahmoud.adly@gmail.com>
2015-03-06 10:49:45 +0200

    swipe and keyboard skeleton

--- js/2048.js
+++ js/2048.js
@@ -11,11 +11,19 @@ $(window).load(function() {
   resizeTouchpad();
   
   $(".touchpad").swipe( {
-    tap:function(event, target) {
-      console.log(event);
-    },
     swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-      console.log("You swiped " + direction ); 
+      console.log(direction);
+      switch(direction) {
+        case "left":
+          break;
+        case "up":
+          break;
+        case "right":
+          break;
+        case "down":
+          break;
+        default:
+      }
     },
     threshold:50
   });
@@ -25,15 +33,35 @@ $(window).bind('resize', function() {
   resizeTouchpad();
 });
 
+$(window).keydown(function(e){
+  switch(e.keyCode) {
+    case 37:
+      // left
+      e.preventDefault();
+      break;
+    case 38:
+      // up
+      e.preventDefault();
+      break;
+    case 39:
+      // right
+      e.preventDefault();
+      break;
+    case 40:
+      // down
+      e.preventDefault();
+      break;
+    default:
+  }
+});
+
+// svg document loading
 (function () {
   var embeds, embed, i, onSvgLoaded;
-
   onSvgLoaded = function () {
-    svg  = $( this.getSVGDocument().documentElement );
+    svg = $( this.getSVGDocument().documentElement );
   };
-
   embeds = document.querySelectorAll('embed');
-
   for (i=0; embed=embeds[i]; i++) {
     embed.addEventListener('load', onSvgLoaded);
   }
