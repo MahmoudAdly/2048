b8ec0aeb3ccd387bdaa2baefca3f5cbf06f8fa72
mahmoud adly <mahmoud.adly@gmail.com>
2015-03-06 17:36:27 +0200

    group variables and functions in one object

--- js/2048.js
+++ js/2048.js
@@ -1,5 +1,114 @@
-var svg = null;
+var board = {
+  svg: null,
+  currentScore: 0,
+  bestScore: 0,
+  array: [[0,0,0,0],
+          [0,0,0,0],
+          [0,0,0,0],
+          [0,0,0,0]
+          ],
+  // get tile and text styles for the matching value
+  selectStyle: function(value) {
+    switch(value) {
+      case 0: return {
+        'tile': { 'fill': '#d7c9c0' },
+        'text': { 'font-size': '0px', 'fill': '#d7c9c0' }
+      }
+      case 2: return {
+        'tile': { 'fill': '#eee4da' },
+        'text': { 'font-size': '55px', 'font-weight': 'bold', 'fill': '#776e65' }
+      }
+      case 4: return {
+        'tile': { 'fill': '#ede0c8' },
+        'text': { 'font-size': '55px', 'font-weight': 'bold', 'fill': '#776e65' }
+      }
+      case 8: return {
+        'tile': { 'fill': '#f2b179' },
+        'text': { 'font-size': '55px', 'font-weight': 'bold', 'fill': '#f9f6f2' }
+      }
+      case 16: return {
+        'tile': { 'fill': '#f59563' },
+        'text': { 'font-size': '55px', 'font-weight': 'bold', 'fill': '#f9f6f2' }
+      }
+      case 32: return {
+        'tile': { 'fill': '#f67c5f' },
+        'text': { 'font-size': '55px', 'font-weight': 'bold', 'fill': '#f9f6f2' }
+      }
+      case 64: return {
+        'tile': { 'fill': '#f65e3b' },
+        'text': { 'font-size': '55px', 'font-weight': 'bold', 'fill': '#f9f6f2' }
+      }
+      case 128: return {
+        'tile': { 'fill': '#edcf72' },
+        'text': { 'font-size': '55px', 'font-weight': 'bold', 'fill': '#f9f6f2' }
+      }
+      case 256: return {
+        'tile': { 'fill': '#edcc61' },
+        'text': { 'font-size': '55px', 'font-weight': 'bold', 'fill': '#f9f6f2' }
+      }
+      case 512: return {
+        'tile': { 'fill': '#edc850' },
+        'text': { 'font-size': '55px', 'font-weight': 'bold', 'fill': '#f9f6f2' }
+      }
+      case 1024: return {
+        'tile': { 'fill': '#edc53f' },
+        'text': { 'font-size': '50px', 'font-weight': 'normal', 'fill': '#f9f6f2' }
+      }
+      case 2048: return {
+        'tile': { 'fill': '#edc22e' },
+        'text': { 'font-size': '50px', 'font-weight': 'normal', 'fill': '#f9f6f2' }
+      }
+      default: return {
+        'tile': { 'fill': '#3c3a32' },
+        'text': { 'font-size': '40px', 'font-weight': 'normal', 'fill': '#f9f6f2' }
+      }  
+    }
+  },
+  // zero all board numbers
+  reset: function() {
+    // clear array
+    for(var i=0; i<4; i++) {
+      for(var j=0; j<4; j++) {
+        this.array[i][j] = 0;
+      }
+    }
+    // clear score
+    this.currentScore = 0;
+  },
+  shift: function() {
 
+  },
+  merge: function() {
+    // body...
+  },
+  hswipe: function() {
+    // body...
+  },
+  vswipe: function() {
+    // body...
+  },
+  // update the svg tiles and score
+  updateView: function() {
+    for(var i=0; i<4; i++) {
+      for(var j=0; j<4; j++) {
+        // get the style matching the value
+        var val = this.array[i][j];
+        var valStyle = this.selectStyle(val);
+
+        // apply style on tile and text
+        this.svg.find('#tile'+i+j).css(valStyle['tile']);
+        this.svg.find('#text'+i+j).css(valStyle['text']);
+
+        // set the text with the value
+        this.svg.find('#text'+i+j).find('tspan').html(val);
+      }
+    }
+    this.svg.find('#textScore').find('tspan').html(this.currentScore);
+    this.svg.find('#textBest').find('tspan').html(this.bestScore);
+  }
+};
+
+// resize the span listening to the swipe events to cover the board
 function resizeTouchpad() {
   $(".touchpad").css('position', 'absolute');
   $(".touchpad").offset($(".stage").offset());
@@ -55,14 +164,10 @@ $(window).keydown(function(e){
   }
 });
 
-// svg document loading
+// load svg object to have access to it
 (function () {
-  var embeds, embed, i, onSvgLoaded;
-  onSvgLoaded = function () {
-    svg = $( this.getSVGDocument().documentElement );
+  var onSvgLoaded = function () {
+    board.svg = $( this.getSVGDocument().documentElement );
   };
-  embeds = document.querySelectorAll('embed');
-  for (i=0; embed=embeds[i]; i++) {
-    embed.addEventListener('load', onSvgLoaded);
-  }
+  document.querySelectorAll('embed')[0].addEventListener('load', onSvgLoaded);
 })();
